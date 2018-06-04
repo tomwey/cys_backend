9909d7e5aeaf2164067fd483089a16c355b10e97
tomwey <tomwey@163.com>
2018-06-04 15:22:53 +0800

    add banner func

---
+++ app/models/banner.rb
@@ -0,0 +1,16 @@
+class Banner < ActiveRecord::Base
+  validates :image, presence: true
+  mount_uploader :image, BannerImageUploader
+  
+  before_create :generate_uid_and_private_token
+  def generate_uid_and_private_token
+    begin
+      n = rand(10)
+      if n == 0
+        n = 8
+      end
+      self.uniq_id = (n.to_s + SecureRandom.random_number.to_s[2..6]).to_i
+    end while self.class.exists?(:uniq_id => uniq_id)
+  end
+  
+end
