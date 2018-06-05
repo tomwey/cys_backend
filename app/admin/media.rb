96f57357d11996c4fe46e9d7abe9c03c3cb11309
tomwey <tomwey@163.com>
2018-06-05 15:28:50 +0800

    add some func

--- app/admin/media.rb
+++ app/admin/media.rb
@@ -17,14 +17,15 @@ form do |f|
   f.semantic_errors
   f.inputs '基本信息' do
     f.input :_type, as: :select, collection: [['电台', 1], ['MV', 2]]
-    f.input :owner_id, as: :select, collection: Performer.where(verified: true).order('id desc').map { |p| [p.name, p.uniq_id] }
-    f.input :title
+    f.input :owner_id, as: :select, collection: Performer.where(verified: true).order('id desc').map { |p| [p.name, p.uniq_id] }, required: true
+    f.input :title, placeholder: '作品标题'
     f.input :cover, hint: '图片建议尺寸为1650x928，格式为：jpg/png/gif'
     f.input :file, hint: '播放文件格式为：mp4/mp3/ogg/wav'
-    f.input :duration
-    f.input :play_url
+    f.input :summary, placeholder: '简要介绍作品'
+    f.input :duration, placeholder: '02:30'
+    # f.input :play_url
     f.input :opened
-    f.input :sort
+    f.input :sort, hint: '值越大显示越靠前'
   end
   actions
 end
