95269f5840628191709f4f224afb43df5e60b808
tomwey <tomwey@163.com>
2018-06-11 09:46:10 +0800

    add some func

--- app/models/vote_item.rb
+++ app/models/vote_item.rb
@@ -2,4 +2,8 @@ class VoteItem < ActiveRecord::Base
   validates :performer_id, presence: true
   validates_uniqueness_of :performer_id, scope: :vote_id
   belongs_to :vote
+  
+  def perform
+    @perform ||= Performer.find_by(uniq_id: self.performer_id)
+  end
 end
