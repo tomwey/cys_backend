9a2060c3123e95ad6598155c8eb2afad795fbded
tomwey <tomwey@163.com>
2018-06-29 11:37:58 +0800

    cccc

--- app/models/vote_view_log.rb
+++ app/models/vote_view_log.rb
@@ -1,2 +1,13 @@
 class VoteViewLog < ActiveRecord::Base
+  after_create :increment_vote_view_count
+  def increment_vote_view_count
+    if vote
+      vote.view_count += 1
+      vote.save!
+    end
+  end
+  
+  def vote
+    @vote ||= Vote.find_by(uniq_id: self.vote_id)
+  end
 end
