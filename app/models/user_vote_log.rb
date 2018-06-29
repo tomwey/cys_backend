ff5245fb5ca5ea16fb4ec58769b1c63b5f7b3555
tomwey <tomwey@163.com>
2018-06-29 15:08:50 +0800

    ccc

--- app/models/user_vote_log.rb
+++ app/models/user_vote_log.rb
@@ -1,2 +1,16 @@
 class UserVoteLog < ActiveRecord::Base
+  after_create :update_vote_count
+  def update_vote_count
+    @vote_items = VoteItem.where(vote_id: self.vote_id, uniq_id: self.answers)
+    @vote_items.each do |item|
+      item.vote_count += 1
+      item.save!
+      
+      if item.vote
+        item.vote.vote_count += 1
+        item.vote.save!
+      end
+      
+    end
+  end
 end
