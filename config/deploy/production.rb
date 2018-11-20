82e9a866142b123e0953047ed86411632baa905e
tomwey <tomwey@163.com>
2018-11-20 10:29:31 +0800

    ccc

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -40,8 +40,8 @@ set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
 set :puma_role, :app
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 5]
-set :puma_workers, 1
+set :puma_threads, [2, 5]
+set :puma_workers, 2
 set :puma_init_active_record, false
 set :puma_preload_app, true
 
